cb0376bb3bb03895c11d41a1d5686f29010c40cf
RahmatSatyawan <rahmat.stywan@gmail.com>
2022-05-29 21:38:40 +0700

    challenge 1 rev

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
 const firstUser = {};
+
 firstUser.name = "Monica";
 firstUser.gender = "Female";
 firstUser.age = "17";
 firstUser.email = "monica@dingdong.com";
 
 const favoriteColorFirst = new Set();
+
 favoriteColorFirst.add("Yellow");
 favoriteColorFirst.add("Pink");
 favoriteColorFirst.add("White");
@@ -19,20 +21,26 @@ firstUser.isHavePet = "Yes";
 const arrEducationFirst = [];
 
 const elementarySchoolFirst = new Map();
+
 elementarySchoolFirst.set("name", "SD 01");
 elementarySchoolFirst.set("city", "Jakarta");
 elementarySchoolFirst.set("graduate", 2016);
+
 arrEducationFirst.push(elementarySchoolFirst);
 
 const juniorSchoolFirst = new Map();
+
 juniorSchoolFirst.set("name", "SD 02");
 juniorSchoolFirst.set("city", "Jakarta");
 juniorSchoolFirst.set("graduate", 2019);
+
 arrEducationFirst.push(juniorSchoolFirst);
 
 const highSchoolFirst = new Map();
+
 highSchoolFirst.set("name", "SMA 03")
 highSchoolFirst.set("city", "Tanggerang")
+
 arrEducationFirst.push(highSchoolFirst);
 
 firstUser.education = arrEducationFirst;
@@ -49,15 +57,18 @@ favoriteRestaurantFirst.add("Padang");
 favoriteRestaurantFirst.add("Tteok");
 favoriteRestaurantFirst.add("Sushi");
 favoriteRestaurantFirst.add("Sushi");
+
 firstUser.favoriteRestaurant = Array.from(favoriteRestaurantFirst);
 
 const secondUser = {};
+
 secondUser.name = "Wendy";
 secondUser.gender = "Male";
 secondUser.age = "23";
 secondUser.email = "wendy@dingdong.com";
 
 favoriteColorSecond = new Set();
+
 favoriteColorSecond.add("Blue");
 favoriteColorSecond.add("Black");
 favoriteColorSecond.add("Grey");
@@ -68,28 +79,36 @@ secondUser.isHavePet = "No";
 const arrEducationSecond = [];
 
 const elementarySchoolSecond = new Map();
+
 elementarySchoolSecond.set("name", "SD 02");
 elementarySchoolSecond.set("city", "Jakarta");
 elementarySchoolSecond.set("graduate", 2010);
+
 arrEducationSecond.push(elementarySchoolSecond);
 
 const juniorSchoolSecond = new Map();
+
 juniorSchoolSecond.set("name", "SD 03");
 juniorSchoolSecond.set("city", "Bogor");
 juniorSchoolSecond.set("graduate", 2013);
+
 arrEducationSecond.push(juniorSchoolSecond);
 
 const highSchoolSecond = new Map();
+
 highSchoolSecond.set("name", "SMA 01");
 highSchoolSecond.set("city", "Surabaya");
 highSchoolSecond.set("graduate", 2016);
+
 arrEducationSecond.push(highSchoolSecond);
 
 const universitySecond = new Map();
+
 universitySecond.set("name", "Universitas Maju");
 universitySecond.set("city", "Tanggerang");
 
 arrEducationSecond.push(universitySecond);
+
 secondUser.education = arrEducationSecond;
 
 favoriteRestaurantSecond = new Set();
@@ -106,8 +125,10 @@ favoriteRestaurantSecond.add("Eggy");
 secondUser.favoriteRestaurant = Array.from(favoriteRestaurantSecond);
 
 // console.log(secondUser.favoriteRestaurant.length);
+
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+users.push(firstUser, secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
